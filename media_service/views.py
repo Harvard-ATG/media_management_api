58385eab6db6466024ea2f35d18af2cb9e5bb2a8
Arthur Barrett <abarrett@fas.harvard.edu>
2015-12-08 14:48:51 -0500

    Got the basic django rest api framework working.

--- media_service/views.py
+++ media_service/views.py
@@ -1,4 +1,23 @@
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from media_service.serializers import UserSerializer, GroupSerializer
 
 def index(request):
-    return HttpResponse('hello world')
+    return HttpResponse('Login to the API <a href="%s">here</a>.<br>Browse the API <a href="%s">here</a>.' % (reverse('rest_framework:login'), reverse('api-root')))
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
