7433f18c29bd9080ea9df5e25ae4151ca72fe793
dodget <tylor_dodge@harvard.edu>
2021-06-21 15:53:12 -0400

    Fix spelling mistake

--- media_management_api/urls.py
+++ media_management_api/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('media_management_api.media_auth.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtian_pair'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('media_management_api.media_service.urls')),
     path('', RedirectView.as_view(url='/api/', permanent=False), name='index'),
